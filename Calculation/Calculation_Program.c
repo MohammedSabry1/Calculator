acdcaba8cddf86f95ed453542f56b68f102bc892
MohammedSabry1 <64170763+MohammedSabry1@users.noreply.github.com>
2024-11-14 20:16:10 +0200

    Update Calculation_Program.c

--- Calculation/Calculation_Program.c
+++ Calculation/Calculation_Program.c
@@ -21,67 +21,79 @@ void KeyPadCalc(){
 	u8 topNum = 0;
 	u8 topOp = 0;
 
-	while(1)
-	{
+	// Function to get the precedence of operators
+	u8 precedence(u8 op) {
+		if (op == 'x' || op == '/') {
+			return 2; // Higher precedence
+		} else if (op == '+' || op == '-') {
+			return 1; // Lower precedence
+		}
+		return 0;
+	}
+
+	// Function to apply an arithmetic operation
+	u16 applyOperation(u16 a, u16 b, u8 op) {
+		if (op == 'x') return a * b;
+		if (op == '/') return a / b;
+		if (op == '+') return a + b;
+		if (op == '-') return a - b;
+		return 0;
+	}
+
+	// Function to evaluate the current stack
+	void evaluate() {
+		u16 b = numStack[--topNum];
+		u16 a = numStack[--topNum];
+		u8 op = operatorStack[--topOp];
+		numStack[topNum++] = applyOperation(a, b, op);
+	}
+
+	while (1) {
 		Key = u8KeyPressed();
-		if(Key != NoKey){
+		if (Key != NoKey) {
 			LcdWriteData(Key);
 
-			// If the key is a digit, form the number
-			if(Key >= '0' && Key <= '9'){
+			// Form the number if the key is a digit
+			if (Key >= '0' && Key <= '9') {
 				num = (num * 10) + (Key - '0');
 			}
 
-			// If the key is an operator, push the current number to the stack and process operators
-			if(Key == '+' || Key == '-' || Key == 'x' || Key == '/'){
-				numStack[topNum++] = num; // Push number to stack
-				num = 0; // Reset number
+			// If the key is an operator (+, -, x, /)
+			if (Key == '+' || Key == '-' || Key == 'x' || Key == '/') {
+				// Push the current number to the stack
+				numStack[topNum++] = num;
+				num = 0;
 
-				// If there's an operator with higher or equal precedence, calculate it
-				while(topOp > 0 && (operatorStack[topOp-1] == 'x' || operatorStack[topOp-1] == '/')){
-					u16 b = numStack[--topNum];
-					u16 a = numStack[--topNum];
-					u8 op = operatorStack[--topOp];
-
-					if(op == 'x'){
-						numStack[topNum++] = a * b;
-					} else if(op == '/'){
-						numStack[topNum++] = a / b;
-					}
+				// Evaluate previous operators with higher or equal precedence
+				while (topOp > 0 && precedence(operatorStack[topOp - 1]) >= precedence(Key)) {
+					evaluate();
 				}
-				operatorStack[topOp++] = Key; // Push operator to stack
+
+				// Push the current operator to the stack
+				operatorStack[topOp++] = Key;
 			}
 
 			// If the key is '=', evaluate the remaining operations
-			if(Key == '='){
-				numStack[topNum++] = num; // Push last number to stack
+			if (Key == '=') {
+				numStack[topNum++] = num;
 
-				// Evaluate the remaining operations
-				while(topOp > 0){
-					u16 b = numStack[--topNum];
-					u16 a = numStack[--topNum];
-					u8 op = operatorStack[--topOp];
-
-					if(op == '+'){
-						numStack[topNum++] = a + b;
-					} else if(op == '-'){
-						numStack[topNum++] = a - b;
-					} else if(op == 'x'){
-						numStack[topNum++] = a * b;
-					} else if(op == '/'){
-						numStack[topNum++] = a / b;
-					}
+				// Evaluate all remaining operators
+				while (topOp > 0) {
+					evaluate();
 				}
 
+				// Display the final result
 				vSetCursor(2, 1);
-				vConvertNumbersToString(numStack[0]); // Final result
-				num = 0; // Reset for the next calculation
+				vConvertNumbersToString(numStack[0]);
+
+				// Reset for the next calculation
+				num = 0;
 				topNum = 0;
 				topOp = 0;
 			}
 
-			// Clear display if 'c' is pressed
-			if(Key == 'c'){
+			// Clear the display if 'c' is pressed
+			if (Key == 'c') {
 				vClearDisplay();
 				num = 0;
 				topNum = 0;
@@ -89,4 +101,8 @@ void KeyPadCalc(){
 			}
 		}
 	}
+
+
 }
+
+
